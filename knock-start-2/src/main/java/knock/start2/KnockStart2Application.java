7705dd6e55e8dbf94e4ad541ea544ea3fbcdb460
knocking77 <knocking@gmail.com>
2022-10-26 15:43:02 +0900

    START

---
+++ knock-start-2/src/main/java/knock/start2/KnockStart2Application.java
@@ -0,0 +1,13 @@
+package knock.start2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnockStart2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnockStart2Application.class, args);
+	}
+
+}
---
+++ knock-start-2/src/test/java/knock/start2/KnockStart2ApplicationTests.java
@@ -0,0 +1,13 @@
+package knock.start2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnockStart2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
